12ba145c5884526f5b09c4dcdd496808d8f32b79
Kenji Mukai <kensuka21@gmail.com>
2018-07-07 16:11:31 -0400

    Update BillsApiApplication.java

--- src/main/java/com/kensuka/bills/BillsApiApplication.java
+++ src/main/java/com/kensuka/bills/BillsApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BillsApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BillsApiApplication.class, args);
 	}
